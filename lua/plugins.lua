08c894ae58424df727ed6b14150622d4d7ad1d0d
smilezouyunhao <smilezouyunhao@163.com>
2022-08-30 23:27:14 +0800

    Packer-nvim installed

---
+++ lua/plugins.lua
@@ -0,0 +1,16 @@
+local status, packer = pcall(require, "packer")
+
+if not status then
+    print("Packer is not installed")
+    return
+end
+
+vim.cmd [[packadd packer.nvim]]
+
+packer.startup(function(use)
+    -- packer可以管理自己
+    use 'wbthomason/packer.nvim'
+
+    -- Plugins列表
+
+end)
